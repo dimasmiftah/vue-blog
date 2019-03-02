22b80d072f9185d9f3e2a266313e2f3786af6f1e
Dimas Miftahul Huda <dimas.mfth@gmail.com>
2019-03-02 20:23:57 +0700

    siap tempur

--- vuejs-playlist/src/App.vue
+++ vuejs-playlist/src/App.vue
@@ -1,11 +1,17 @@
 <template>
   <div>
-    
+
   </div>
 </template>
 
 <script>
+import formOne from './components/formOne.vue';
+import formTwo from './components/formTwo.vue';
 export default {
+  components: {
+    'formOne' : formOne,
+    'formTwo' : formTwo
+  },
   data () {
     return {
      
