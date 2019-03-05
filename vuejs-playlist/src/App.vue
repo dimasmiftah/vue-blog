e132b8ea993e391b45626b50aa06b9e83d375493
Dimas Miftahul Huda <dimas.mfth@gmail.com>
2019-03-05 14:18:17 +0700

    done

--- vuejs-playlist/src/App.vue
+++ vuejs-playlist/src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-
+    <keep-alive>
+      <component v-bind:is="component"></component>
+    </keep-alive>
+    <button v-on:click="component = 'form-one'">Show form one</button>
+    <button v-on:click="component = 'form-two'">Show form two</button>
   </div>
 </template>
 
@@ -9,12 +13,12 @@ import formOne from './components/formOne.vue';
 import formTwo from './components/formTwo.vue';
 export default {
   components: {
-    'formOne' : formOne,
-    'formTwo' : formTwo
+    'form-one' : formOne,
+    'form-two' : formTwo
   },
   data () {
     return {
-     
+     component: 'form-two'
     }
   }
 }
