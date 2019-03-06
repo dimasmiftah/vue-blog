73ace7832859aaa49ae7dae8f24ef51e7ebd416d
Dimas Miftahul Huda <dimas.mfth@gmail.com>
2019-03-06 16:06:11 +0700

    done

--- vuejs-playlist/src/App.vue
+++ vuejs-playlist/src/App.vue
@@ -1,11 +1,15 @@
 <template>
   <div>
-    
+    <add-blog></add-blog>
   </div>
 </template>
 
 <script>
+import addBlog from './components/addBlog.vue';
 export default {
+  components: {
+    'add-blog' : addBlog
+  },
   data () {
     return {
      
